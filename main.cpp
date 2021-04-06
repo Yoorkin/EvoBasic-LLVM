279b8b1e3a55504447c3dd2c7e2e6855bd63951b
Yorkin <937697555@qq.com>
2021-04-06 17:32:13 +0800

    集成llvm

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include<iostream>
+using namespace std;
+
 #include<antlr4-runtime/antlr4-runtime.h>
 #include"antlr/BasicLexer.h"
 #include"antlr/BasicParser.h"
 #include"antlr/BasicBaseListener.h"
 #include"antlr/BasicListener.h"
-using namespace std;
 using namespace antlr4;
+
+#include<llvm/IR/DerivedTypes.h>
+#include<llvm/IR/IRBuilder.h>
+#include<llvm/IR/Function.h>
+#include<llvm/IR/InstrTypes.h>
+#include<llvm/IR/Instruction.h>
+#include<llvm/IR/LLVMContext.h>
+#include<llvm/IR/Module.h>
+#include<llvm/IR/Verifier.h>
+#include<llvm/Support/raw_ostream.h>
+#include<llvm/Target/TargetMachine.h>
+#include<llvm/ExecutionEngine/JITSymbol.h>
+#include<llvm/ExecutionEngine/Orc/LLJIT.h>
+#include<llvm/ExecutionEngine/Orc/CompileUtils.h>
+#include<llvm/ExecutionEngine/Orc/Core.h>
+#include<llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h>
+#include<llvm/ExecutionEngine/Orc/ExecutionUtils.h>
+#include<llvm/ExecutionEngine/Orc/IRCompileLayer.h>
+#include<llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#include<llvm/ExecutionEngine/SectionMemoryManager.h>
+#include<llvm/IR/DataLayout.h>
+#include<llvm/ExecutionEngine/ExecutionEngine.h>
+using namespace llvm;
+
 class Listener:public BasicListener{
     virtual void enterExp(BasicParser::ExpContext *ctx)override{
         
@@ -75,4 +100,14 @@ int main(int argc, const char* argv[]){
     tree::ParseTree *tree = parser.statement();
     Listener listener;
     tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+
+    LLVMInitializeNativeTarget();
+    LLVMInitializeNativeAsmPrinter();
+    LLVMContext context;
+    Module mod("test",context);
+
+    Function *fun = Function::Create(FunctionType::get(Type::getInt32Ty(context),false),Function::ExternalLinkage,"main",mod);
+    BasicBlock *block = BasicBlock::Create(context, "EntryBlock", fun);
+    mod.print(llvm::outs(),nullptr,false);
+
 }
