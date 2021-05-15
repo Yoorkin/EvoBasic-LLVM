9561240148177041a3fd317aeb40f5255d707b84
Yorkin <937697555@qq.com>
2021-05-15 17:18:21 +0800

    gtest能够编译运行了

--- test/basicStmtTest.cpp
+++ test/basicStmtTest.cpp
@@ -3,11 +3,11 @@
 //
 #include<gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions) {
-// Expect two strings not to be equal.
-EXPECT_STRNE("hello", "world");
-// Expect equality.
-EXPECT_EQ(7 * 6, 42);
+TEST(BasicStmtTest, Add) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
 }
 
 //#include<iostream>
