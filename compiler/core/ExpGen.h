c0384bc03ba28bb1805285c334d58d01fe5698c3
Yorkin <937697555@qq.com>
2021-05-25 16:19:47 +0800

    添加一些基本类型typedef

--- compiler/core/ExpGen.h
+++ compiler/core/ExpGen.h
@@ -10,84 +10,92 @@
 #include"../antlr/BasicParser.h"
 #include"../antlr/BasicBaseVisitor.h"
 
-class ConstExpVisitor:public BasicBaseVisitor {
-    bool inConstExp=false;
-public:
-    virtual antlrcpp::Any visitConstExp(BasicParser::ConstExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
+#include"genUtility.h"
+namespace classicBasic{
 
-    virtual antlrcpp::Any visitBucketExp(BasicParser::BucketExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitMulExp(BasicParser::MulExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitNegExp(BasicParser::NegExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitRefExp(BasicParser::RefExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitPowModExp(BasicParser::PowModExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitLogicExp(BasicParser::LogicExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitBitExp(BasicParser::BitExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitLogicNotExp(BasicParser::LogicNotExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitPluExp(BasicParser::PluExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitCmpExp(BasicParser::CmpExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
+    class ConstExpVisitor:public BasicBaseVisitor {
+        CodeGenerator& gen;
+        GenerateUnit& unit;
+    public:
+        ConstExpVisitor(GenerateUnit& unit):unit(unit),gen(unit.gen){}
+        virtual antlrcpp::Any visitConstExp(BasicParser::ConstExpContext *ctx)override{
+            uint64_t value = visit(ctx->exp()).as<uint64_t>();
+            return ConstantInt::get(Type::getInt64Ty(gen.context),value);
+        }
+        virtual antlrcpp::Any visitMulExp(BasicParser::MulExpContext *ctx) override {
+            uint64_t left = visit(ctx->left).as<uint64_t>();
+            uint64_t right = visit(ctx->right).as<uint64_t>();
+            return left*right;
+        }
+        virtual antlrcpp::Any visitNegExp(BasicParser::NegExpContext *ctx) override {
+            uint64_t right = visit(ctx->right).as<uint64_t>();
+            return -right;
+        }
+        virtual antlrcpp::Any visitRefExp(BasicParser::RefExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitPowModExp(BasicParser::PowModExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitLogicExp(BasicParser::LogicExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitBitExp(BasicParser::BitExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitLogicNotExp(BasicParser::LogicNotExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitPluExp(BasicParser::PluExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitCmpExp(BasicParser::CmpExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
 
 
-    virtual antlrcpp::Any visitTerminateNodeExp(BasicParser::TerminateNodeExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
+        virtual antlrcpp::Any visitTerminateNodeExp(BasicParser::TerminateNodeExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
 
-    virtual antlrcpp::Any visitInteger(BasicParser::IntegerContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitDecimal(BasicParser::DecimalContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitString(BasicParser::StringContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitBoolean(BasicParser::BooleanContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitFunctionCall(BasicParser::FunctionCallContext *ctx) override {
-        return visitChildren(ctx);
-    }
+        virtual antlrcpp::Any visitInteger(BasicParser::IntegerContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitDecimal(BasicParser::DecimalContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitString(BasicParser::StringContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitBoolean(BasicParser::BooleanContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitFunctionCall(BasicParser::FunctionCallContext *ctx) override {
+            return visitChildren(ctx);
+        }
 
-    virtual antlrcpp::Any visitTargetExp(BasicParser::TargetExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitMapExp(BasicParser::MapExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitArrayExp(BasicParser::ArrayExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitTupleExp(BasicParser::TupleExpContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitArrayOffset(BasicParser::ArrayOffsetContext *ctx) override {
-        return visitChildren(ctx);
-    }
-    virtual antlrcpp::Any visitLambda(BasicParser::LambdaContext *ctx) override {
-        return visitChildren(ctx);
-    }
-};
+        virtual antlrcpp::Any visitTargetExp(BasicParser::TargetExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitMapExp(BasicParser::MapExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitArrayExp(BasicParser::ArrayExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitTupleExp(BasicParser::TupleExpContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitArrayOffset(BasicParser::ArrayOffsetContext *ctx) override {
+            return visitChildren(ctx);
+        }
+        virtual antlrcpp::Any visitLambda(BasicParser::LambdaContext *ctx) override {
+            return visitChildren(ctx);
+        }
+    };
+
+}
+
 
 
 #endif //CLASSICBASIC_EXPGEN_H
---
+++ compiler/core/basicTypeDef.h
@@ -0,0 +1,22 @@
+//
+// Created by yorkin on 5/24/21.
+//
+
+#ifndef CLASSICBASIC_BASICTYPEDEF_H
+#define CLASSICBASIC_BASICTYPEDEF_H
+namespace classicBasic{
+    typedef signed long i64;
+    typedef signed int i32;
+    typedef signed short i16;
+    typedef signed char i8;
+
+    typedef unsigned long u64;
+    typedef unsigned int u32;
+    typedef unsigned short u16;
+    typedef unsigned char u8;
+
+    typedef float f32;
+    typedef double f64;
+}
+
+#endif //CLASSICBASIC_BASICTYPEDEF_H
